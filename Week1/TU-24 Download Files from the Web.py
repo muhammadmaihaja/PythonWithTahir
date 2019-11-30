48e2512f610a1ec3607ebab67a5eaecae14537cf
Muhammad Maihaja <muhammadmaihaja@gmail.com>
2019-11-30 01:37:52 +0100

    Updated

--- Week1/TU-24 Download Files from the Web.py
+++ Week1/TU-24 Download Files from the Web.py
@@ -1,15 +1,15 @@
 from urllib import request
-goog_url = "https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1543525807&period2=1575061807&interval=1d&events=history&crumb=/0R7y2X4gKj"
+stockData = "https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1543525807&period2=1575061807&interval=1d&events=history&crumb=/0R7y2X4gKj"
 
-def download_stock_data(csv_url):
+def downloadStockInfo(csv_url):
     response = request.urlopen(csv_url)
     csv = response.read()
     csv_str = str(csv)
-    lines = csv_str.split("\\n")
-    dest_url = r'goog.csv'
-    fx = open(desturl, 'w')
-    for line in lines:
+    formatted = csv_str.split("\\n")
+    filename = r'stockData.csv'
+    fx = open(filename, 'w')
+    for line in formatted:
         fx.write(line + "\n")
     fx.close()
 
-download_stock_data(goog_url)
+downloadStockInfo(stockData)
