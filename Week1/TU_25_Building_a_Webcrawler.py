f015fe4c4398d7a039f46e835b31bc417c29886f
Muhammad Maihaja <muhammadmaihaja@gmail.com>
2019-11-30 09:13:06 +0100

    Webcrawler from Bucky vids

---
+++ Week1/TU_25_Building_a_Webcrawler.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+def spider(max_pages):
+    page = 1
+    while page <= max_pages:
+        url = "www.lindaikejisblog.com/page/" + str(page)
+        source_code = requests.get(url)
+        plaintext_source_code = source_code.text
+        formatted_source_code = BeautifulSoup(plaintext_source_code)
+        for links in formatted_source_code.findAll('a', {'class' : 'story_title'}):
+            href = links.get('href')
+            title = links.string
+            print(title)
+            print(href, "\n")
+    page += 1
+
+spider(1)
