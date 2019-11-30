b7c7e2e067cdfaee8f64b30b38fd7e7f4c948642
Muhammad Maihaja <muhammadmaihaja@gmail.com>
2019-11-30 10:07:53 +0100

    Webcrawler from Bucky vids

--- Week1/TU_25_Building_a_Webcrawler.py
+++ Week1/TU_25_Building_a_Webcrawler.py
@@ -1,18 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def spider(max_pages):
     page = 1
     while page <= max_pages:
-        url = "www.lindaikejisblog.com/page/" + str(page)
+        url = "https://www.lindaikejisblog.com/page/" + str(page)
         source_code = requests.get(url)
         plaintext_source_code = source_code.text
-        formatted_source_code = BeautifulSoup(plaintext_source_code)
-        for links in formatted_source_code.findAll('a', {'class' : 'story_title'}):
+        formatted_source_code = BeautifulSoup(plaintext_source_code, features="html.parser")
+        for links in formatted_source_code.findAll('a', {'class': 'story_title'}):
             href = links.get('href')
             title = links.string
             print(title)
             print(href, "\n")
     page += 1
 
-spider(1)
+
+spider(2)
